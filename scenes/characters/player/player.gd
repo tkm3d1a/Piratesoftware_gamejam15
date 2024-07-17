9bef20aad79bdf96aeaa031c54a95c7e6a3158b4
Tim Klimpel <tim.klimpel@gmail.com>
2024-07-17 14:51:05 -0400

    remove comments

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -5,23 +5,18 @@ class_name Player extends CharacterBody2D
 
 var can_transform: bool = false
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready() -> void:
 	speed = speed * 100
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed * delta
